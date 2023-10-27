24f19c9b34cb1c76ff0bd399be536a491f5e59a5
Radmir Musabaev <musabaevradmir@gmail.com>
2023-10-28 00:10:06 +0400

    настроил и стилизовал компонент с кнопками с цифрами

---
+++ src/components/PhoneNumber/PhoneNumberInput.tsx
@@ -0,0 +1,42 @@
+import React, {FC, useRef, useState} from 'react';
+import classes from "./PhoneNumberInput.module.css"
+
+interface Props {
+    handleNumberClick(digit: string): void;
+    handleBackspaceClick(): void;
+    buttonsRef: React.MutableRefObject<(HTMLButtonElement | null)[]>;
+    handleButtonKeyDown(event: React.KeyboardEvent<HTMLButtonElement>, index: number): void;
+    focusedButtonIndex: number | null;
+}
+
+const PhoneNumberInput: FC<Props> = ({handleNumberClick, handleBackspaceClick, buttonsRef, handleButtonKeyDown, focusedButtonIndex}) => {
+
+
+    return (
+        <div className={classes.button_holder}>
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].map((item, index) => (
+                <button
+                    ref={(el) => (buttonsRef.current[index] = el)}
+                    key={item}
+                    onClick={() => handleNumberClick(item.toString())}
+                    onKeyDown={(e) => handleButtonKeyDown(e, index)}
+                    tabIndex={0}
+                    className={`${classes.number_button} ${focusedButtonIndex === index ? classes.focused_button : ''}`}
+                >
+                    {item}
+                </button>
+            ))}
+            <button
+                ref={(el) => (buttonsRef.current[10] = el)}
+                className={`${classes.backspace_button} ${focusedButtonIndex === 10 ? classes.focused_button : ''}`}
+                onClick={handleBackspaceClick}
+                onKeyDown={(e) => handleButtonKeyDown(e, 10)}
+                tabIndex={0}
+            >
+                СТЕРЕТЬ
+            </button>
+        </div>
+    );
+};
+
+export default PhoneNumberInput;
